c166cedc1ef01ec6f2115b90addf0bd8607071e2
Harold Schreckengost <tug@alleghenycyclery.com>
2014-02-10 13:10:02 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ACycleryTools::Application.config.secret_key_base = '6f638252c30659229b9d94d4b822479ec154b945936a6606d159ac8b9ae728035785ea611ff7008ee02184ac67ba495fd7975324e06d34c2661b6a26db24be41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ACycleryTools::Application.config.session_store :cookie_store, key: '_ACycleryTools_session'
